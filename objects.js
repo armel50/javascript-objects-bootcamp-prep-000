5cd5249e1749111a8743242371520f352df252a5
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 06:14:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
-var playlist={
- artist:"keff"
-};
+// var playlist={
+// artistName:"keff"
+// };
 
-function updatePlaylist (playlist, artistName, songTitle){
-   playlist.artistName= songTitle;
- return playlist;
+// function updatePlaylist (playlist, artistName, songTitle){
+//   playlist.artistName= songTitle;
+// return playlist;
 
-}
+// }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
-}
+// function removeFromPlaylist(playlist, artistName){
+//   delete playlist.artistName;
+// }
